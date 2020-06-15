d478515068e2e549ec7ce3a793762c84693870e9
Jayashri <jayashri1394@gmail.com>
2020-06-15 16:33:08 +0200

    loginkk

--- pizza_page/urls.py
+++ pizza_page/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.main_view, name="main_view"),
+    path("/", views.main_view, name="main_view"),
     path("<int:orderid>/", views.order, name="order"),
     path("orders/", views.orders_list, name="orders_list"),
     path("order_placement/", views.order_confirmation, name="order_confirmation"),
--- pizza_page/views.py
+++ pizza_page/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Food, Price, OrderItem, Order
+
 # Create your views here.
 def main_view(request):
 
