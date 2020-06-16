1992a7d2a5c96a58b02d86d9680f3071cfd807ef
Jayashri <jayashri1394@gmail.com>
2020-06-16 02:59:55 +0200

    changed to postgres to eleminate the disaapearance of images in heroku

--- pizza_app/settings.py
+++ pizza_app/settings.py
@@ -79,9 +79,12 @@ WSGI_APPLICATION = 'pizza_app.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'jayashri94',
+        'HOST': 'localhost',
     }
 }
 
