b6932899b8b7627be303ca323fa4c1ec8be9d743
Jayashri <jayashri1394@gmail.com>
2020-06-16 03:06:54 +0200

    changed to sqlite reversed

--- pizza_app/settings.py
+++ pizza_app/settings.py
@@ -79,12 +79,9 @@ WSGI_APPLICATION = 'pizza_app.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mydb',
-        'USER': 'myuser',
-        'PASSWORD': 'jayashri94',
-        'HOST': 'localhost',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
