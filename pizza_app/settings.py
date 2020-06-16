35e272628c1452be9c737cbe2efaca81b90d76e7
Jayashri <jayashri1394@gmail.com>
2020-06-16 03:42:08 +0200

    postgres

--- pizza_app/settings.py
+++ pizza_app/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'pizza_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'jayashri94',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
